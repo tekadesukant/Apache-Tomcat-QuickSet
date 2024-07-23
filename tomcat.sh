980933b5b9ecbc249359b3ee2427f6a5d22c9097
Tekade Sukant <59193514+tekadesukant@users.noreply.github.com>
2024-07-23 15:36:36 +0530

    Update tomcat.sh

--- tomcat.sh
+++ tomcat.sh
@@ -9,25 +9,25 @@ TOMCAT_VERSION=11.0.0
 TOMCAT_URL="https://dlcdn.apache.org/tomcat/tomcat-$MAJOR_VERSION/v$TOMCAT_VERSION/bin/apache-tomcat-$TOMCAT_VERSION.tar.gz"
 TOMCAT_URLL="https://dlcdn.apache.org/tomcat/tomcat-$MAJOR_VERSION/v$TOMCAT_VERSION-M22/bin/apache-tomcat-$TOMCAT_VERSION-M22.tar.gz"
 
-echo "Fetching Tomcat version ${TOMCAT_VERSION} from ${TOMCAT_URL}"
+echo "Fetching Tomcat version $TOMCAT_VERSION from $TOMCAT_URL"
 
 
 # Download and extract Tomcat
 wget $TOMCAT_URL
 wget $TOMCAT_URLL
-tar -zxvf apache-tomcat-${TOMCAT_VERSION}.tar.gz
+tar -zxvf apache-tomcat-$TOMCAT_VERSION.tar.gz
 
 read -p "Enter password for Tomcat user: " password
 echo
 
 # Configure Tomcat users
-sed -i '56  a\<role rolename="manager-gui"/>' apache-tomcat-${TOMCAT_VERSION}/conf/tomcat-users.xml
-sed -i '57  a\<role rolename="manager-script"/>' apache-tomcat-${TOMCAT_VERSION}/conf/tomcat-users.xml
-sed -i '58  a\<user username="apachetomcat" password="'"$password"'" roles="manager-gui,manager-script"/>' apache-tomcat-${TOMCAT_VERSION}/conf/tomcat-users.xml
-sed -i '59  a\</tomcat-users>' apache-tomcat-${TOMCAT_VERSION}/conf/tomcat-users.xml
-sed -i '56d' apache-tomcat-${TOMCAT_VERSION}/conf/tomcat-users.xml
-sed -i '21d' apache-tomcat-${TOMCAT_VERSION}/webapps/manager/META-INF/context.xml
-sed -i '22d' apache-tomcat-${TOMCAT_VERSION}/webapps/manager/META-INF/context.xml
+sed -i '56  a\<role rolename="manager-gui"/>' apache-tomcat-$TOMCAT_VERSION/conf/tomcat-users.xml
+sed -i '57  a\<role rolename="manager-script"/>' apache-tomcat-$TOMCAT_VERSION/conf/tomcat-users.xml
+sed -i '58  a\<user username="apachetomcat" password="'"$password"'" roles="manager-gui,manager-script"/>' apache-tomcat-$TOMCAT_VERSION/conf/tomcat-users.xml
+sed -i '59  a\</tomcat-users>' apache-tomcat-$TOMCAT_VERSION/conf/tomcat-users.xml
+sed -i '56d' apache-tomcat-$TOMCAT_VERSION/conf/tomcat-users.xml
+sed -i '21d' apache-tomcat-$TOMCAT_VERSION/webapps/manager/META-INF/context.xml
+sed -i '22d' apache-tomcat-$TOMCAT_VERSION/webapps/manager/META-INF/context.xml
 
 # Start Tomcat
-sh apache-tomcat-${TOMCAT_VERSION}/bin/startup.sh
+sh apache-tomcat-$TOMCAT_VERSION/bin/startup.sh
