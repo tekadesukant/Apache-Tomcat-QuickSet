cfcc100c3fb36aae42f25f23aed918ae76666714
Tekade Sukant <59193514+tekadesukant@users.noreply.github.com>
2024-07-29 20:13:18 +0530

    Update tomcat.sh

--- tomcat.sh
+++ tomcat.sh
@@ -5,6 +5,17 @@ amazon-linux-extras install java-openjdk11 -y
 MAJOR_VERSION=11
 TOMCAT_VERSION=11.0.0-M22
 
+wget https://download.java.net/java/GA/jdk17.0.2/dfd4a8d0985749f896bed50d7138ee7f/8/GPL/openjdk-17.0.2_linux-x64_bin.tar.gz
+tar xvf openjdk-17.0.2_linux-x64_bin.tar.gz
+sudo mv jdk-17.0.2/ /opt/jdk-17
+sudo tee /etc/profile.d/jdk.sh <<EOF
+export JAVA_HOME=/opt/jdk-17
+export PATH=\$PATH:\$JAVA_HOME/bin
+EOF
+
+source /etc/profile.d/jdk.sh
+
+
 # Construct the download URL
 TOMCAT_URL="https://dlcdn.apache.org/tomcat/tomcat-$MAJOR_VERSION/v$TOMCAT_VERSION/bin/apache-tomcat-$TOMCAT_VERSION.tar.gz"
 
