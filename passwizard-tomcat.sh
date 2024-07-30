46e9ee3030ee3ad0fd0ff7dde44ef3d18f3f9d2a
Tekade Sukant <59193514+tekadesukant@users.noreply.github.com>
2024-07-31 00:29:16 +0530

    Update passwizard-tomcat.sh

--- passwizard-tomcat.sh
+++ passwizard-tomcat.sh
@@ -4,26 +4,13 @@
 echo "Enter new Tomcat manager password (minimum 6 characters): "
 read CUSTOM_TOMCAT_PASSWD
 
-# Validate password (minimum 8 characters)
-if [ ${#CUSTOM_TOMCAT_PASSWD} -lt 6 ]; then
-    echo "Password must be at least 6 characters long."
-    exit 1
-fi
-
 # Update the password in tomcat-users.xml
-sudo sed -i 's/password="[^"]*"/password="'"$CUSTOM_TOMCAT_PASSWD"'"/' /opt/tomcat/conf/tomcat-users.xml
+sudo sed -i '58  c <user username="apachetomcat" password="'"$CUSTOM_TOMCAT_PASSWD"'" roles="manager-gui,manager-script"/>' /opt/tomcat/conf/tomcat-users.xml
 
 # Update the password in tomcatcreds.txt
-sudo sed -i 's/password="[^"]*"/password="'"$CUSTOM_TOMCAT_PASSWD"'"/' /opt/tomcatcreds.txt
+sudo sed -i '2 c password="'"$CUSTOM_TOMCAT_PASSWD"'"' /opt/tomcatcreds.txt
 
-# Check if the updates were successful
-if [ $? -eq 0 ]; then
-    echo "Password successfully updated."
+echo "Password successfully updated."
 
-    # Optionally restart Tomcat to apply the new password
-    sudo tomcat -restart
-
-else
-    echo "Failed to update password."
-    exit 1
-fi
+# Optionally restart Tomcat to apply the new password
+sudo tomcat -restart
