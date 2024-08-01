61775c5b54cd7101f31a06f678db043ce88811a7
Tekade Sukant <59193514+tekadesukant@users.noreply.github.com>
2024-08-02 04:28:27 +0530

    Update portuner-tomcat.sh

--- portuner-tomcat.sh
+++ portuner-tomcat.sh
@@ -1,20 +1,14 @@
 #!/bin/bash
-# TESTED SUCCESFULLY FOR UBUNTU INSTANCE
 # Prompt the user to enter a new port number
 echo "Enter new port number (1024-65535): "
 read CUSTOM_TOMCAT_PORT
 
 # Update the port number in server.xml
-sudo sed -i 's/port="8080"/port="'"$CUSTOM_TOMCAT_PORT"'"/' /opt/tomcat/conf/server.xml
+sudo sed -i ' /<Connector port/  c \ \ \ \ <Connector port="'$CUSTOM_TOMCAT_PORT'" protocol="HTTP/1.1" '  /opt/tomcat/conf/server.xml
 
 # Update the portnumber in tomcatcreds.txt
-sudo sed -i '4 c portnumber="'"$CUSTOM_TOMCAT_PORT"'"' /opt/tomcatcreds.txt
+sudo sed -i '4 c portnumber='$CUSTOM_TOMCAT_PORT' ' /opt/tomcreds.txt
 
-echo "Port number successfully updated to "'"$CUSTOM_TOMCAT_PORT"'". "
+echo "Port number successfully updated to "'$CUSTOM_TOMCAT_PORT'". "
+echo "Restart tomcat (comm: tomcat --restart) to apply chnages"
 
-# Optionally, check Tomcat service status
-if systemctl is-active --quiet tomcat; then
-    echo "Tomcat is currently running. Please restart Tomcat (comm: tomcat -restart) to apply changes."
-else
-    echo "Tomcat is not running. You can start Tomcat (comm: tomcat -start) to apply the new port number."
-fi
