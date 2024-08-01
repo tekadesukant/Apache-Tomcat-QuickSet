546aac174bf8382732259078b341380b586a88bf
Tekade Sukant <59193514+tekadesukant@users.noreply.github.com>
2024-08-02 04:29:12 +0530

    Update remove-tomcat.sh

--- remove-tomcat.sh
+++ remove-tomcat.sh
@@ -4,4 +4,7 @@ sleep 10
 sudo rm -r /opt/tomcat/
 sudo rm -r /opt/jdk-17/
 sudo rm -r /usr/local/sbin/tomcat
+sudo rm -f /opt/tomcreds.txt
+sudo rm -f /opt/portuner.sh
+sudo rm -f /opt/passwd.sh
 echo "Tomcat removed successfully from the machine"
