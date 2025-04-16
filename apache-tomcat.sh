ed46080d5340ed8338b4b3c9204fb1a49894d2b3
Tekade Sukant <59193514+tekadesukant@users.noreply.github.com>
2025-04-16 12:25:17 +0530

    Update apache-tomcat.sh

--- apache-tomcat.sh
+++ apache-tomcat.sh
@@ -176,8 +176,8 @@ sudo tee /opt/remove.sh <<'EOF'
 #!/bin/bash
 sudo /opt/tomcat/bin/shutdown.sh
 sleep 10
-sudo rm -R /opt/tomcat -f
-sudo rm -R /opt/jdk-17 -f
+sudo rm -r /opt/tomcat/
+sudo rm -r /opt/jdk-17/
 sudo rm -r /usr/local/sbin/tomcat
 sudo rm -f /opt/tomcreds.txt
 sudo rm -f /opt/portuner.sh
