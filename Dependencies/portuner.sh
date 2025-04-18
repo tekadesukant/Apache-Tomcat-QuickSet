7792a247f364a671d17aaf8b4a51c07d4f5527fc
Tekade Sukant <59193514+tekadesukant@users.noreply.github.com>
2025-04-18 10:50:09 +0530

    Update portuner.sh

--- Dependencies/portuner.sh
+++ Dependencies/portuner.sh
@@ -4,11 +4,13 @@ echo "Enter new port number (1024-65535): "
 read CUSTOM_TOMCAT_PORT
 
 # Update the port number in server.xml
+echo "Updating port in server.xml..."
 sudo sed -i ' /<Connector port/  c \ \ \ \ <Connector port="'$CUSTOM_TOMCAT_PORT'" protocol="HTTP/1.1" '  /opt/tomcat/conf/server.xml
 
 # Update the portnumber in tomcatcreds.txt
+echo "Updating port number in tomcatcreds.txt..."
 sudo sed -i '4 c portnumber='$CUSTOM_TOMCAT_PORT' ' /opt/tomcreds.txt
 
 echo "Port number successfully updated to "'$CUSTOM_TOMCAT_PORT'". "
-echo "Restart tomcat (comm: tomcat --restart) to apply chnages"
+echo "Restart Tomcat using (comm: tomcat --restart) to apply the changes"
 
