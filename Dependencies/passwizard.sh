76ee463b113febcd6dc5ef7c882601494df6e923
Tekade Sukant <59193514+tekadesukant@users.noreply.github.com>
2025-04-18 10:52:18 +0530

    Update passwizard.sh

--- Dependencies/passwizard.sh
+++ Dependencies/passwizard.sh
@@ -5,12 +5,15 @@ echo "Enter new Tomcat manager password (minimum 6 characters): "
 read CUSTOM_TOMCAT_PASSWD
 
 # Update the password in tomcat-users.xml
+echo "Updating password in tomcat-users.xml..."
 sudo sed -i '58  c <user username="apachetomcat" password="'"$CUSTOM_TOMCAT_PASSWD"'" roles="manager-gui,manager-script"/>' /opt/tomcat/conf/tomcat-users.xml
 
 # Update the password in tomcatcreds.txt
+echo "Updating password in tomcatcreds.txt..."
 sudo sed -i '2 c password="'"$CUSTOM_TOMCAT_PASSWD"'"' /opt/tomcatcreds.txt
 
 echo "Password successfully updated."
 
 # Optionally restart Tomcat to apply the new password
+# echo "Restarting Tomcat to apply changes..."
 # sudo tomcat -restart
